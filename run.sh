c4926100ef8b64c71d1b35c3720bccec46df1ba4
Kwanchanok.H <Kwanchanok.H@ibmdt.co.th>
2025-04-19 21:42:37 +0700

    first commit

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+nohup python -u run.py \
+    --test_file ./data/tasks_test.jsonl \
+    --api_key "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" \
+    --headless \
+    --max_iter 15 \
+    --max_attached_imgs 3 \
+    --temperature 1 \
+    --fix_box_color \
+    --seed 42 > test_tasks.log &
